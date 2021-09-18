90c008a3d0457bf3677220d64d21abc018392185
LightSapfier <minemaximum@gmail.com>
2021-09-18 14:09:24 +0300

    init commit and Vanya task

---
+++ ObcConverter.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def ConvertObs(game_state_turn) -> np.array(dtype = float):
+    """
+    Converts game_state.turn.map and game_state.turn.players to numpy float array 
+    Resulting array contains full info about state, has 3 dimensions
+    
+    Full list of layers with explained meanings:
+    1. Resource Type
+    2. Resource Val
+    3. Unit type map?
+    4...
+    """
+    pass
+
+    #assert obc_vector.ndim == 3
